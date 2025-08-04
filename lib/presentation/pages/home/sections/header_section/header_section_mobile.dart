c94c6e1cb766f60a2c6b8916c49c0d8ea7695624
timothymutesasira <timothy.mutesasira@raisingthevillage.org>
2025-08-04 16:05:07 +0300

    Reset remote with local code, v2 website

---
+++ lib/presentation/pages/home/sections/header_section/header_section_mobile.dart
@@ -0,0 +1,234 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:nimbus/presentation/layout/adaptive.dart';
+import 'package:nimbus/presentation/pages/home/sections/header_section/widgets.dart';
+import 'package:nimbus/presentation/widgets/buttons/nimbus_button.dart';
+import 'package:nimbus/presentation/widgets/content_area.dart';
+import 'package:nimbus/presentation/widgets/spaces.dart';
+import 'package:nimbus/values/values.dart';
+
+const double bodyTextSizeLg = 16.0;
+const double bodyTextSizeSm = 14.0;
+const double socialTextSizeLg = 18.0;
+const double socialTextSizeSm = 14.0;
+const double sidePadding = Sizes.PADDING_16;
+
+class HeaderSectionMobile extends StatefulWidget {
+  const HeaderSectionMobile({Key? key}) : super(key: key);
+
+  @override
+  _HeaderSectionMobileState createState() => _HeaderSectionMobileState();
+}
+
+class _HeaderSectionMobileState extends State<HeaderSectionMobile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 20),
+      vsync: this,
+    )..repeat();
+
+    _controller.forward();
+    _controller.addListener(() {
+      if (_controller.status == AnimationStatus.completed) {
+        _controller.reset();
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    double headerIntroTextSize = Sizes.TEXT_SIZE_24;
+    double screenWidth = widthOfScreen(context) - (sidePadding * 2);
+    double contentAreaWidth = screenWidth;
+    TextStyle? bodyTextStyle =
+        textTheme.bodyLarge?.copyWith(fontSize: bodyTextSizeSm);
+    TextStyle? socialTitleStyle =
+        textTheme.labelMedium?.copyWith(fontSize: socialTextSizeSm);
+
+    double buttonWidth = 80;
+    double buttonHeight = 48;
+
+    double sizeOfBlobSm = screenWidth * 0.4;
+    double sizeOfGoldenGlobe = screenWidth * 0.3;
+    double dottedGoldenGlobeOffset = sizeOfBlobSm * 0.4;
+    double heightOfBlobAndGlobe =
+        computeHeight(dottedGoldenGlobeOffset, sizeOfGoldenGlobe, sizeOfBlobSm);
+    double heightOfStack = heightOfBlobAndGlobe * 2;
+    double blobOffset = heightOfStack * 0.3;
+    return ContentArea(
+      child: Stack(
+        children: [
+          Container(
+            height: heightOfStack,
+            child: Stack(
+              children: [
+                Stack(
+                  children: [
+                    // Positioned(
+                    //   left: -(sizeOfBlobSm * 0.7),
+                    //   top: blobOffset,
+                    //   child: Image.asset(
+                    //     ImagePath.BLOB_BLACK,
+                    //     height: sizeOfBlobSm,
+                    //     width: sizeOfBlobSm,
+                    //   ),
+                    // ),
+                    Positioned(
+                      left: -(sizeOfGoldenGlobe / 3),
+                      top: blobOffset + dottedGoldenGlobeOffset,
+                      child: RotationTransition(
+                        turns: _controller,
+                        child: Image.asset(
+                          ImagePath.DOTS_GLOBE_YELLOW,
+                          width: sizeOfGoldenGlobe,
+                          height: sizeOfGoldenGlobe,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  right: -(sizeOfBlobSm),
+                  child: HeaderImage(
+                    controller: _controller,
+                    globeSize: sizeOfGoldenGlobe,
+                    imageHeight: heightOfStack,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: heightOfStack * 0.1),
+                    child: SelectableText(
+                      StringConst.FIRST_NAME,
+                      style: textTheme.headlineLarge?.copyWith(
+                        color: AppColors.grey50,
+                        fontSize: headerIntroTextSize * 2.5,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: sidePadding),
+                    margin: EdgeInsets.only(top: heightOfStack * 0.3),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ConstrainedBox(
+                              constraints:
+                                  BoxConstraints(maxWidth: screenWidth),
+                              child: AnimatedTextKit(
+                                animatedTexts: [
+                                  TypewriterAnimatedText(
+                                    StringConst.INTRO,
+                                    speed: Duration(milliseconds: 60),
+                                    textStyle: textTheme.headlineMedium?.copyWith(
+                                      fontSize: headerIntroTextSize,
+                                      color: AppColors.primaryColor,
+                                    ),
+                                  ),
+                                ],
+                                onTap: () {},
+                                isRepeatingAnimation: true,
+                                totalRepeatCount: 5,
+                              ),
+                            ),
+                            ConstrainedBox(
+                              constraints:
+                                  BoxConstraints(maxWidth: screenWidth),
+                              child: AnimatedTextKit(
+                                animatedTexts: [
+                                  TypewriterAnimatedText(
+                                    StringConst.POSITION,
+                                    speed: Duration(milliseconds: 80),
+                                    textStyle: textTheme.headlineMedium?.copyWith(
+                                      fontSize: headerIntroTextSize,
+                                      color: AppColors.secondaryColor,
+                                      height: 1.2,
+                                    ),
+                                  ),
+                                ],
+                                onTap: () {},
+                                isRepeatingAnimation: true,
+                                totalRepeatCount: 5,
+                              ),
+                            ),
+                            SpaceH16(),
+                            ConstrainedBox(
+                              constraints:
+                                  BoxConstraints(maxWidth: screenWidth * 0.5),
+                              child: SelectableText(
+                                StringConst.ABOUT_DEV,
+                                style: bodyTextStyle?.copyWith(
+                                  height: 1.5,
+                                  // color: AppColors.black,
+                                ),
+                              ),
+                            ),
+                            SpaceH40(),
+                            Row(
+                              children: [
+                                NimbusButton(
+                                  width: buttonWidth,
+                                  height: buttonHeight,
+                                  buttonTitle: StringConst.DOWNLOAD_CV,
+                                  onPressed: () {},
+                                ),
+                                SpaceW16(),
+                              ],
+                            ),
+                            SpaceH30(),
+                            Wrap(
+                              children: buildSocialIcons(Data.socialData),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SpaceH40(),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: sidePadding,
+                ),
+                child: Column(
+                  children: buildCardRow(
+                    context: context,
+                    data: Data.nimbusCardData,
+                    width: contentAreaWidth,
+                    isHorizontal: false,
+                    hasAnimation: false,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/values/colors.dart
@@ -0,0 +1,58 @@
+part of values;
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF39a132);
+  //Shades of primary color
+  static const Color primary200 = Color(0xFF39a132);
+  static const Color primary300 = Color(0xFF39a132);
+
+  static const Color secondaryColor = Color(0xff30439b);
+
+  //accent Colors
+  static const Color accentColor = Color(0xFFF7F7F7);
+  static const Color accentColor100 = Color(0xFFF2F0F1);
+
+  //Accent colors
+  static const Color primaryText1 = Color(0xFF959595);
+  static const Color primaryText2 = Color(0xFF717171);
+
+  //black
+  static const Color black = Color(0xFF000000);
+  static const Color black400 = Color(0xFF212121);
+  static const Color black100 = Color(0xFF313131);
+  static const Color black200 = Color(0xFF2D2D2D);
+
+  //White
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color offWhite = Color(0xFFF3F3F3);
+  static const Color darkWhite = Color(0xFFDFDFDF);
+  static const Color offWhite200 = Color(0xFFE5E5DB);
+
+  //Blue
+  static const Color blue300 = Color(0xff30439b);
+
+  //Green
+  static const Color green100 = Color(0xFFC8E6C9);
+  static const Color green200 = Color(0xFFA5D6A7);
+  static const Color green300 = Color(0xFF81C784);
+  static const Color green400 = Color(0xFF66BB6A);
+  static const Color green500 = Color(0xFF4CAF50);
+  static const Color green600 = Color(0xFF43A047);
+  static const Color green700 = Color(0xFF388E3C);
+  static const Color green800 = Color(0xFF2E7D32);
+  static const Color green900 = Color(0xFF1B5E20);
+
+  //Pink
+  static const Color pink300 = Color(0xFFDC5687);
+
+  //Grey
+  static const Color grey50 = Color(0xFFF2F1F1);
+  static const Color grey70 = Color(0xFFEDECEC);
+  static const Color grey100 = Color(0xFFE7E7E7);
+  static const Color grey150 = Color(0xFFC6C6C6);
+  static const Color grey250 = Color(0xFFBBBBBB);
+  static const Color grey300 = Color(0xFFBABABA);
+  static const Color grey350 = Color(0xFFADADAD);
+  //Red
+  static const Color red = Color(0xFFF1291A);
+}
